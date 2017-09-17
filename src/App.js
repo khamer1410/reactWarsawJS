904276b00fabc4d6e73b47cba0a22102d09af067
Krzysztof Hamerszmit <kkhamer@gmail.com>
2017-09-17 14:18:02 +0200

    list

--- src/App.js
+++ src/App.js
@@ -3,17 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
 import TransactionCard from './components/transaction-cards';
+import StateLess from './components/hello-stateless';
+import TransactionList from './components/transaction-list';
 
-
-function App2(props) {
-  return <button onClick={props.func}> {props.name}</button>    //wywolanie funkcji rodzica
-}
 
 class App extends Component {
   
   constructor(props) {
     super(props);
     this.state={
+        transactions: [
+          {
+            id: 1,
+            description: 'Potwierdzenie uczestnictwa w warsztatach WarsawJS',
+            value: 20,
+            date: '16.09.2017',
+            category: 'Edukacja'
+          },
+          {
+            id: 2,
+            description: 'Bilet na pociąg',
+            value: 120,
+            date: '07.09.2017',
+            category: 'Transport'
+          },
+          {
+            id: 3,
+            description: 'Części do samochodu',
+            value: 430,
+            date: '26.08.2017',
+            category: 'Samochód'
+          }
+        ],
       counter: 0,
       lista: ["a", "b", "c"]  // <div>{this.state.lista}</div> wyświetla cala liste! czesciej uzywamy do wyswietlenia listy elementowDOM
     }
@@ -38,19 +59,35 @@ class App extends Component {
         <button onClick={this.clickCounter}> Click me </button>
         {/* <div>{this.state.lista}</div>  */}
         <ul>{myList}</ul>
+        <br />
 
         <div>
-          <h1>Counter component</h1>
+          <h1 className="emphesize">Counter component</h1>
           <Counter />
+        </div>
+        <br />
+        
+        <div>
+          <StateLess name={"KTOŚ"}/>
+          <h1 className="emphesize">Transaction Card component</h1>
+          <TransactionCard item= {this.state.transactions[1]} />
         </div>
 
         <div>
-          <h1>Transaction Card component</h1>
-          <TransactionCard item= "hej" />
+          <h1 className="emphesize">Transaction List component</h1>
+          <TransactionList
+            items={this.state.transactions}
+            onRemoveTransaction = {this.handleRemoveTransactions}
+          />
         </div>
 
       </div>
     );
+  }
+
+  handleRemoveTransactions = ({ id }) => {
+    const { transactions } = this.state;  //decompositin - z obiektu this.state wybierz transactions
+    this.setState({ transactions: transactions.filter(transaction => transaction.id !== id) });
   }
 
   clickCounter = ()=> {
--- src/components/transaction-cards.js
+++ src/components/transaction-cards.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';   //osobna biblioteka ktora sie wydzielila = definiuje wszystkie props prop.types.number name itp
 
-const TransactionCard = ({ item: { description, value, date, category }}) => (
+const TransactionCard = ({ item: { description, value, date, category, id }, onRemoveTransaction}) => (
   <div className="Transaction-card">
     <h1>{description}</h1>
     <h2>{value} zl</h2>
@@ -9,6 +9,7 @@ const TransactionCard = ({ item: { description, value, date, category }}) => (
       <span>{date}</span>
       <span>{category}</span>
     </div>
+    <button onClick={onRemoveTransaction}>X</button>  
   </div>
 );
 
